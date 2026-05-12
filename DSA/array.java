b952c787bc1b5ed67bfae80876e6465fa4e09ace
Vivek adile <vivekadile700@gmail.com>
2026-05-12 15:18:06 +0530

    learned array today

--- DSA/array.java
+++ DSA/array.java
@@ -1,11 +1,56 @@
 package DSA;
+import java.util.*;
 
 public class array {
     public static void main(String[] args){
-        int a[]={2,3,4,5,6,6,6};
-        for (int i=0;i<5;i++){
-            System.out.println(a[i]);
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the number of elements in array");
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int sum=0, count=0,max=-99;
+        System.out.println("enter the numbers"+n+" in array ");
+
+
+        for (int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+            
+
         } 
+        System.out.println("1.the array is :-");
+        
+
+        for (int i=0;i<n;i++){
+            System.out.println(a[i]);
+            sum=sum+a[i];
+            count+=1;
+            if (a[i]>max){
+                max=a[i];
+            }
+            
+
+        } 
+        System.out.println("4.Array in reverse order");
+         for (int i=n;i<0;i--){
+            System.out.println(a[i]);
+            
+            
+
+        } 
+
+
+        System.out.println("2.Sum is "+sum);
+        System.out.println("3.number of elements in"+count);
+        System.out.println("4.max number is "+max);
+        System.out.println("5.Array in reverse order");
+         for (int i=n;i<0;i--){
+            System.out.println(a[i]);
+            
+            
+
+        } 
+
+
+
     }
     
 }
