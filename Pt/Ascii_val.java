6544d05286ae0beba66e8da239bae80c54b488d8
Vivek adile <vivekadile700@gmail.com>
2026-06-02 11:53:25 +0530

    Placement train day 1

---
+++ Pt/Ascii_val.java
@@ -0,0 +1,10 @@
+
+
+public class Ascii_val {
+    public static void main(String[] args) {
+        char l='b';
+        int a=l;
+        System.out.println(a);
+    }
+    
+}
---
+++ Pt/multiline.java
@@ -0,0 +1,11 @@
+public class multiline  {
+    public static void main (String[] args)
+    {
+        System.out.println("Minimu of byte"+Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+        
+        //System.out.println("Name-Vivek \n Age-21 \n ss");
+    }
+}
