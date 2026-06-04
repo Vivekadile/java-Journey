dc2198946f2bee723207d95eda7bbd1225bbc959
Vivek adile <vivekadile700@gmail.com>
2026-06-04 13:05:47 +0530

    made some changes

--- DSA/date_day.java
+++ DSA/date_day.java
@@ -1,19 +1,26 @@
 
 
 public class date_day {
-    int day =06 , month=5, yyyy=2004;
+    public static void main(String[] args)
+     {
+    int day = 7 , month=10, yyyy=2004;
 
     int monthcode[]={0,3,3,6,1,4,6,2,5,0,3,5};
-    String din[]={m,t,w,th,f,sa,su};
-    int daycode= day%7;
-    int mc=monthcode[month];
+    String[]  daycode={"Sun","Mon","tue","wed","thu","Fri","Sat"};
+    int mc=monthcode[month-1];
     int yc=yyyy%100;
 
+    int leap=0;
 
+    if (yyyy % 4 == 0 && (month == 1 || month == 2))
+    {
+        leap = -1;
+    }
 
+    int result=(day+mc+6+yc+leap)%7;
 
-
-
-
+    System.out.println(daycode[result]);
+        
+    }
     
 }
